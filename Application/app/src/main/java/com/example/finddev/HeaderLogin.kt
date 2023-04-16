a684676005abdaa92a4f87a01e1273b532d0de00
Carlos Felix <carlos.fsilva@bandtec.com.br>
2023-04-16 20:56:30 -0300

    Tela de login

--- Application/app/src/main/java/com/example/finddev/HeaderLogin.kt
+++ Application/app/src/main/java/com/example/finddev/HeaderLogin.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Header_Login.newInstance] factory method to
+ * Use the [HeaderLogin.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Header_Login : Fragment() {
+class HeaderLogin : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -54,7 +54,7 @@ class Header_Login : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Header_Login().apply {
+            HeaderLogin().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
