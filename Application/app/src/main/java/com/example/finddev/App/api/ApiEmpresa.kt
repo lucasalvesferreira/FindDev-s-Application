5e8714776c79f235a6e9661a1f38e766ceef845c
unknown <lucas.aferreira@bandtec.com.br>
2023-04-23 20:14:56 -0300

    feat - arquivos mobile kt

---
+++ Application/app/src/main/java/com/example/finddev/App/api/ApiEmpresa.kt
@@ -0,0 +1,10 @@
+package com.example.finddev.App.api
+
+interface ApiEmpresa {
+
+
+
+
+
+
+}
---
+++ Application/app/src/main/java/com/example/finddev/App/api/Apis.kt
@@ -0,0 +1,20 @@
+package com.example.finddev.App.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Apis {
+    var BASE_URL = "https://ec2-44-214-40-119.compute-1.amazonaws.com:8080/api/v1"
+
+    // função que cria uma instância de cliente da Api de autenticação
+    fun getApiUsuarios() : ApiDesenvolvedor {
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+        return retrofit.create(ApiDesenvolvedor::class.java)
+    }
+
+
+
+}
