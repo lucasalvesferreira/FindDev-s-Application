404ec969d317f542f374a89003a977637e7aa9e6
Paulo Gabriel Andrade de Souza <paulo.gabriel.souza@accenture.com>
2023-04-23 01:27:37 -0300

    Telas cadastro construidas

--- Application/app/src/main/java/com/example/finddev/Header_Cadastro.kt
+++ Application/app/src/main/java/com/example/finddev/Header_Cadastro.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Header_Login.newInstance] factory method to
+ * Use the [Header_Cadastro.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Header_Login : Fragment() {
+class Header_Cadastro : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +34,7 @@ class Header_Login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_header__login, container, false)
+        return inflater.inflate(R.layout.fragment_header__cadastro, container, false)
     }
 
     fun navegarLogin(componente: View){
@@ -54,7 +53,7 @@ class Header_Login : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Header_Login().apply {
+            Header_Cadastro().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
