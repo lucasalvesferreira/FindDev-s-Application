3d00800eceb14c0550d251b2636fcbf2d7196383
unknown <lucas.aferreira@bandtec.com.br>
2023-04-23 23:44:22 -0300

    alteraÃcoes

--- Application/app/src/main/java/com/example/finddev/App/api/ApiEmpresa.kt
+++ Application/app/src/main/java/com/example/finddev/App/api/ApiEmpresa.kt
@@ -1,8 +1,13 @@
 package com.example.finddev.App.api
 
+import com.example.finddev.App.model.EmpresaModel
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiEmpresa {
 
-
+    @GET("/empresa")
+    fun getAllCompanys() : Call<List<EmpresaModel>>
 
 
 
